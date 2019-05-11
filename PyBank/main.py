d83fde2fc0fbe533e926230116ec7808855d639b
kurmaine <kurmaine.m@gmail.com>
2019-05-11 14:04:12 -0400

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,87 @@
+#HOMEWORK
+# The total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The average of the changes in "Profit/Losses" over the entire period
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
+
+
+# First we'll import the os module
+import os
+
+# Module for reading CSV files
+import csv
+
+# This will allow us to create file paths across operating systems
+csvpath = os.path.join('budget_data.csv')
+
+numberlist= list()
+
+
+# Improved Reading using CSV module
+
+with open(csvpath, newline="") as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+ 
+    csv_header = next(csvreader)
+
+    print(csvreader)
+
+    mylist = list(csvreader)
+   
+   
+
+month_count = 0
+total_revenue = 0
+this_month_revenue = 0
+last_month_revenue = 0
+revenue_change = 0
+revenue_changes = []
+months = []
+
+
+    # Read each row of data after the header
+for row in mylist:
+        #print(row)
+        #print(len(row(0)))
+        #print(row[1])
+
+        total = int(row[1])
+        numberlist.append(total)
+
+        month_count = month_count + 1
+        months.append(row[0])
+        this_month_revenue = int(row[1])
+        total_revenue = total_revenue + this_month_revenue
+        if month_count > 1:
+            revenue_change = this_month_revenue - last_month_revenue
+            revenue_changes.append(revenue_change)
+        last_month_revenue = this_month_revenue
+    
+
+
+
+# analyze the month by month results
+sum_rev_changes = sum(revenue_changes)
+average_change = sum_rev_changes / (month_count - 1)
+max_change = max(revenue_changes)
+min_change = min(revenue_changes)
+max_month_index = revenue_changes.index(max_change)
+min_month_index = revenue_changes.index(min_change)
+max_month = months[max_month_index]
+min_month = months[min_month_index]
+
+print(f"Total Months: {month_count}")
+print(f"Total Revenue: ${total_revenue}")
+print(f"Average Revenue Change: ${average_change}")
+print(f"Greatest Increase in Revenue: {max_month} (${max_change})")
+print(f"Greatest Decrease in Revenue: {min_month} (${min_change})")      
+
+
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,97 @@
+#HOMEWORK
+# The total number of votes cast
 
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote.
+
+
+# First we'll import the os module
+import os
+
+# Module for reading CSV files
+import csv
+
+bank_data = {}
+values_list = []
+revenue_change = []
+date_relabel =[]
+
+csvpath = os.path.join('election_data.csv')
+
+#Creates dictionary to be used for candidate name and vote count.
+poll = {}
+
+#Sets variable, total votes, to zero for count.
+total_votes = 0
+
+with open(csvpath, newline="") as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+ 
+    csv_header = next(csvreader)
+
+    print(csvreader)
+
+    mylist = list(csvreader)
+
+
+
+for row in mylist:
+    total_votes += 1
+    if row[2] in poll.keys():
+            poll[row[2]] = poll[row[2]] + 1
+    else:
+        poll[row[2]] = 1
+
+print("Below is the total votes casted")
+print(total_votes)
+print("Below are the votes in relation to the candudates")
+print(poll)
+
+#create empty list for candidates and his/her vote count
+candidates = []
+num_votes = []
+
+#takes dictionary keys and values and, respectively, dumps them into the lists, 
+# candidates and num_votes
+for key, value in poll.items():
+    candidates.append(key)
+    num_votes.append(value)
+
+# creates vote percent list
+vote_percent = []
+for n in num_votes:
+    vote_percent.append(round(n/total_votes*100, 1))
+
+print("Below are all the candidates who received votes.")
+print(candidates)
+
+print("Below are the percentage of votes in relation to the candidates.")
+print(vote_percent)
+
+# zips candidates, num_votes, vote_percent into tuples
+clean_data = list(zip(candidates, num_votes, vote_percent))
+
+#creates winner_list to put winners (even if there is a tie)
+winner_list = []
+
+for name in clean_data:
+    if max(num_votes) == name[1]:
+        winner_list.append(name[0])
+
+# makes winner_list a str with the first entry
+winner = winner_list[0]
+
+#only runs if there is a tie and puts additional winners into a string separated by commas
+if len(winner_list) > 1:
+    for w in range(1, len(winner_list)):
+        winner = winner + ", " + winner_list[w]
+
+
+print("The winner is... " + str(winner))
